8fcb878aa8475952f2448c89a75ab484944fd945
Nathan W <nathan@frcv.net>
2014-10-30 08:37:03 -0500

    Scheduling alerts is working, as well as sending to redqueen

--- index.js
+++ index.js
@@ -1,34 +1,79 @@
 var util = require('util')
 var ical = require('ical')
 var moment = require('moment')
+var request = require('request');
+
+// 1 Hour
+var pollInt = 60 * 60 * 1001;
 
 //ical.fromURL('http://256.makerslocal.org/calendar.ics', {}, parseData);
 parseData('',ical.parseFile('/home/jimshoe/dev/makerslocal/node_ical_twitter/calendar.ics'));
 
 setInterval(function(){
-    ical.fromURL('http://256.makerslocal.org/calendar.ics', {}, parseData);
-}, 60 * 60 * 1000);      
+    console.log(util.format('Parsing ical @ %s\n', moment().format())); 
+    parseData('',ical.parseFile('/home/jimshoe/dev/makerslocal/node_ical_twitter/calendar.ics'));
+    //ical.fromURL('http://256.makerslocal.org/calendar.ics', {}, parseData);
+}, pollInt);      
 
 function parseData(err, data){
   for (var k in data){
     if (data.hasOwnProperty(k)) {
       var ev = data[k]
-      var alertTimes = [
-          moment(ev.start).subtract(1, 'hour'), 
-          moment(ev.start).subtract(1, 'day'),
-          moment(ev.start).subtract(7, 'day')
-          ]
-      for (i = 0; i < alertTimes.length; i++) { 
-        scheduleAlert(ev, alertTimes[i]);
+      if (ev.type == "VEVENT" ){
+        var alertTimes = [
+            moment(ev.start), 
+            moment(ev.start).subtract(1, 'hour'), 
+            moment(ev.start).subtract(1, 'day'),
+            moment(ev.start).subtract(7, 'day')
+            ]
+        for (i = 0; i < alertTimes.length; i++) { 
+          scheduleAlert(ev, alertTimes[i]);
+        }
       }
     }
   }
 }
 
 function scheduleAlert(ev, alertTime){
-  if (ev.type == "VEVENT" && (alertTime > moment() && alertTime.subtract(1, 'hour') < moment())) {
-    printEvent(ev);
+  var now = moment();
+  if (alertTime > now && alertTime - pollInt < now) {
+    console.log(util.format('Schedule: %s %s for alerting at %s',
+      ev.summary,
+      moment(ev.start).format(),
+      alertTime.format()));
+     
+    setTimeout(function(){
+      sendIrc(ev);
+      sendTweet(ev);
+    }, alertTime - now);      
   }
+}
+
+function sendIrc(ev){
+  console.log("[DEBUG] - Sending irc");
+  var post_data = JSON.stringify({
+      'message' : util.format('New Event: %s will be happending at %s',
+        ev.summary,
+        moment(ev.start).format()),
+      'channel': '##rqtest',
+      'isaction': false,
+      'key' : '13371234'
+  });
+  request.post(
+      { headers:{'Content-Type' : 'application/json'},
+        url:'https://restirc.tylercrumpton.com/relay',
+        body: post_data
+      },
+        function (error, response, body) {
+              console.log(response.statusCode);
+        } 
+   
+  );
+}
+
+function sendTweet(ev){
+  console.log("[DEBUG] - Sending tweet");
+  printEvent(ev);
 }
 
 function printEvent(ev){
